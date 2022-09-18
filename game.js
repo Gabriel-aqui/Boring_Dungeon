08cd3656cad5e3eecc759ddf238632d0e9f11d15
Gabriel Zahra M. Gradim <91337220+Gabriel-aqui@users.noreply.github.com>
2022-09-18 09:27:51 -0300

    v3.0

---
+++ game.js
@@ -0,0 +1,13 @@
+class Game {
+    constructor() {}
+    start() {
+        //playson = new Player()
+        /*playerSprite = createSprite(100, 30)
+        playerSprite.addAnimation("playerImage", player)
+        playerSprite.addAnimation("player2Image", player2)
+        zombieSprite = createSprite(30, 30)
+        zombieSprite.addAnimation("zombieImage", zombie)
+        zombieSprite.addAnimation("zombie2Image", zombie2)*/
+    }
+    
+}
--- sketch.js
+++ sketch.js
@@ -18,6 +18,12 @@ function preload() {
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
+  playerSprite = createSprite(100, 30)
+  playerSprite.addAnimation("playerImage", player)
+  playerSprite.addAnimation("player2Image", player2)
+  zombieSprite = createSprite(30, 30)
+  zombieSprite.addAnimation("zombieImage", zombie)
+  zombieSprite.addAnimation("zombie2Image", zombie2)
   game = new Game()
   game.start()
 }
